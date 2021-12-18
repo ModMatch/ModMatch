bda202d51389ea1383661551c0f5e5578546784e
TzeJit <51875344+tzejit@users.noreply.github.com>
2021-12-18 13:28:45 +0800

    Add leaving confirmed group functionality

--- src/components/Group/Group.js
+++ src/components/Group/Group.js
@@ -1,10 +1,14 @@
 import React, {useState, useEffect} from 'react';
+import { Navigate, useNavigate } from 'react-router-dom';
 import Api from '../../Api';
 
 function Group(props) {
 
   const [groups, setGroups] = useState([]);
   const [loading, setLoading] = useState(true);
+
+  const navigate = useNavigate();
+
   useEffect(()=>{
     setLoading(true);
     async function getGroups() {
@@ -20,6 +24,19 @@ function Group(props) {
     setLoading(false);
   }, [])
 
+  const onLeaveButClick = async (e) => {
+    const id = e.target.dataset.groupid;
+    navigate(0);
+    await Api({
+      method: 'put',
+      url: `/confirmedgroups/${id}`,
+      headers: {
+        Authorization: localStorage.getItem("Authorization")
+      },
+      data: {userid : props.curruserid}
+    })
+  }
+
   if (loading) {
     return ("loading..");
   }
@@ -27,7 +44,7 @@ function Group(props) {
   return (
     <div>
       {groups.map(e => { return (
-        <div>
+        <div key={e._id}>
           Title {e.title} <br/>
           Desc {e.description} <br/>
           Members <br/>
@@ -35,6 +52,7 @@ function Group(props) {
             <ul>{u.email}</ul>
           )
           })}
+        <button data-groupid={e._id} onClick={onLeaveButClick}>Leave</button>
         </div>
       )})}
     </div>
